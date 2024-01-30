5375641ae577660a4e876f27361d3148998d0fed
MauricioMahecha <124183411+MaoProgram@users.noreply.github.com>
2024-01-30 12:31:47 -0500

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservationComponent,
+    ReservationListComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
